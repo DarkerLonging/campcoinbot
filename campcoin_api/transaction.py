35de2de28da791a225bd49f51e69f46875b52a33
Andrew <darkerlonging@gmail.com>
2018-07-04 15:47:40 -0600

    replace default transaction

--- campcoin_api/transaction.py
+++ campcoin_api/transaction.py
@@ -1,18 +1,21 @@
-from lib.keys import signData, verifyData
+from . import keys
+signData = keys.signData
+verifyData = keys.verifyData
 import base64
 
 class Transaction:
-    def __init__(self, sender, reciever, amount, signature=None):
+    def __init__(self, sender, reciever, amount, signature=None, private = None):
         self.sender = sender
         self.reciever = reciever
         self.amount = amount
+        self.private = private
         if signature==None:
             self.signature = self.signTransaction()
         else:
             self.signature = signature
 
     def signTransaction(self):
-        signature = signData(str(self.sender) + str(self.reciever) + str(self.amount))
+        signature = signData(str(self.sender) + str(self.reciever) + str(self.amount), self.private)
         return base64.b64encode(signature)
 
     def verifyTransaction(self, public_key_string):
@@ -21,4 +24,7 @@ class Transaction:
                     base64.b64decode(self.signature))
 
     def _asdict(self):
-        return self.__dict__
+        print(self.signature)
+        data = self.__dict__.copy()
+        del data['private']
+        return data
