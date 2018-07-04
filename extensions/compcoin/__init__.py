90fe12145203aa4b28f77be81180af4b1b8b731c
Andrew <darkerlonging@gmail.com>
2018-07-04 16:21:29 -0600

    fix collection

--- extensions/compcoin/__init__.py
+++ extensions/compcoin/__init__.py
@@ -83,7 +83,7 @@ class CompCoin(bot.Extension):
                 private_key = SigningKey.from_pem(keyjson[str(message.author.id)]["private"])
                 public_key = VerifyingKey.from_pem(keyjson[str(266918350645362688)]["public"])
                 publickey = str(base64.b64encode(public_key.to_string()), "utf-8")
-                trx = transaction.Transaction(sender_publickey, publickey, ctx.args.amount * 0.1, None, private_key)
+                trx = transaction.Transaction(sender_publickey, publickey, ctx.args.amount + (ctx.args.amount * 0.1), None, private_key)
                 cc.postTransaction(trx)
                 if ctx.args.user.id != 266918350645362688:
                     public_key = VerifyingKey.from_pem(keyjson[str(266918350645362688)]["public"])
@@ -97,7 +97,6 @@ class CompCoin(bot.Extension):
             else:
                 private_key = SigningKey.from_pem(keyjson[str(266918350645362688)]["private"])
                 public_key = VerifyingKey.from_pem(keyjson[str(ctx.args.user.id)]["public"])
-                sender_publickey = str(base64.b64encode(public_key.to_string()), "utf-8")
                 publickey = str(base64.b64encode(public_key.to_string()), "utf-8")
                 trx = transaction.Transaction(sender_publickey, publickey, ctx.args.amount, None, private_key)
                 cc.postTransaction(trx)
